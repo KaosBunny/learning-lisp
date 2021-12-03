d81e44c9e453dfa1f5895f3f29f27f23e18a804b
KaosBunny <Niamh.K.Ferns@gmail.com>
2021-12-03 23:15:52 +1300

    Learning ifelse and multiple statements per branch.

--- conditionals-3.lisp
+++ conditionals-3.lisp
@@ -1,1 +1,16 @@
-;;; This is where I'll learn when and unless.
+;;; If else and multiple statements in an if...
+(defvar x (read))
+(defvar y (read))
+
+;; (Progn args) can be used to pass multiple lines into the branch of a conditional statement.
+(if (= x y)
+    (progn
+      (format t "This was written in one line... ~%Would you like to print your number? ~%")
+      (setf x (read))
+      (if (= x 1)
+          (format t "Your number is ~s." x)))
+    (progn ;; This is the else statement of the if c:
+      (format t "They weren't equal... Would you like to print them anyway? ~%")
+      (setf x (read))
+      (if (= x 1)
+          (format t "Your number is ~s." x))))
